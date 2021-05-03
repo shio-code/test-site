ac1fbb54889ef0376d8c5bfebe2a39b967198b30
shio-code <74956257+shio-code@users.noreply.github.com>
2021-05-03 11:01:53 +0900

    Add files via upload

---
+++ js/main.js
@@ -0,0 +1,10 @@
+'use strict';
+
+{
+  const btn = document.getElementById('btn');
+  
+  btn.addEventListener('click',function(){
+    document.getElementById('title').classList.add('changed');
+  });
+  
+}
